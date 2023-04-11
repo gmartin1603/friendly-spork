6e9ce97c35145bdf2a0ca1aa5d62a936c60f95e7
George <duramaxer228@gmail.com>
2023-04-10 21:44:16 -0500

    Clean up & Added checkbox check to handleChange

--- src/components/SegInput.js
+++ src/components/SegInput.js
@@ -3,62 +3,57 @@ import { useAuthState } from '../context/auth/AuthProvider';
 
 function SegInput({name, segs, slots, styling, setSegs, dir, width, txtSize, id }) {
 
-    const [{formObj, view, shifts},dispatch] = useAuthState()
+    const [{ formObj },dispatch] = useAuthState()
 
     const handleChange = (e) => {
-        console.log(e.target.name+' '+e.target.type)
-
+        // console.log(e.target.name+' '+e.target.type)
         let value = ''
         if(e.target.value) {
             value = `${e.target.value[0].toUpperCase()}${e.target.value.slice(1)}`
         }
 
         let update = {}
-        switch (e.target.id) {
-            case "forced":
-                if (e.target.checked) {
-                    if (segs[e.target.name].trade) {
-                        update = {...segs[e.target.name], [e.target.id]: e.target.checked, trade: false}
-                    } else {
-                        update = {...segs[e.target.name], [e.target.id]: e.target.checked}
-                    }
-                } else {
-                    update = {...segs[e.target.name], [e.target.id]: e.target.checked}
-                }
-            break
-            case "trade":
-                if (e.target.checked) {
-                    if (segs[e.target.name].forced) {
-                        update = {...segs[e.target.name], [e.target.id]: e.target.checked, forced: false}
-                    } else {
-                        update = {...segs[e.target.name], [e.target.id]: e.target.checked}
-                    }
-                } else {
-                    update = {...segs[e.target.name], [e.target.id]: e.target.checked}
-                }
-            break
-            default:
-                console.log(e.target.id)
-        }
         if (e.target.type === 'checkbox') {
+            switch (e.target.id) {
+                case "forced":
+                    if (e.target.checked) {
+                        if (segs[e.target.name].trade) {
+                            update = {...segs[e.target.name], [e.target.id]: e.target.checked, trade: false}
+                        } else {
+                            update = {...segs[e.target.name], [e.target.id]: e.target.checked}
+                        }
+                    } else {
+                        update = {...segs[e.target.name], [e.target.id]: e.target.checked}
+                    }
+                break
+                case "trade":
+                    if (e.target.checked) {
+                        if (segs[e.target.name].forced) {
+                            update = {...segs[e.target.name], [e.target.id]: e.target.checked, forced: false}
+                        } else {
+                            update = {...segs[e.target.name], [e.target.id]: e.target.checked}
+                        }
+                    } else {
+                        update = {...segs[e.target.name], [e.target.id]: e.target.checked}
+                    }
+                break
+                default:
+                    console.log(e.target.id)
+            }
 
         } else {
             update = {...segs[e.target.name], [e.target.id]: value}
         }
         if (slots) {
-            console.log(update)
+            // console.log(update)
             setSegs({name:e.target.name, id: id, load: update})
         } else {
             setSegs({name:e.target.name, load: update})
         }
     }
 
-    const styles = {
-        main: styling,
-    }
-
     return (
-        <div className={styles.main}>
+        <div className={styling}>
         <h3 className={`font-bold text-${txtSize}`}>
         { formObj.shift.segs[name]}
         </h3>
