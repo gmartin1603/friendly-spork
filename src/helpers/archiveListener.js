16a7048fe24d3517d84adda131bfb8a21d8fc4ba
George <duramaxer228@gmail.com>
2023-04-20 19:44:49 -0500

    Removed white space and log messages

--- src/helpers/archiveListener.js
+++ src/helpers/archiveListener.js
@@ -6,10 +6,9 @@ const useArchiveListener = (coll, date) => {
     const [archive, setArchive] = useState(null)
 
     useEffect(() => {
-        console.log(coll, date)
         const q = query(doc(db, coll, 'rota', 'archive', date))
         const unsubscribe = onSnapshot(q, (qSnap) => {
-            console.log(qSnap.data())
+            // console.log(qSnap.data())
             if (qSnap.exists) {
                 setArchive(qSnap.data())
             } else {
@@ -19,8 +18,7 @@ const useArchiveListener = (coll, date) => {
         return () => unsubscribe()
     },[coll, date])
 
-    console.log(archive)
-
+    // console.log(archive)
     return archive
 }
 
