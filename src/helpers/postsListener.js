270f2d00d2e1d200abb10ba348f3aa72fc8de71b
George <duramaxer228@gmail.com>
2023-05-12 14:57:32 -0500

    Added user check to reduce log out erros

--- src/helpers/postsListener.js
+++ src/helpers/postsListener.js
@@ -1,51 +1,60 @@
-import { collection, where, onSnapshot, orderBy, query } from "firebase/firestore";
+import {
+  collection,
+  where,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { useAuthState } from "../context/auth/AuthProvider";
 import { db } from "../firebase/firestore";
 
 const usePostsListener = (dept) => {
-    const [{cols, count, today}, dispatch] = useAuthState()
+  const [{ cols, count, today, profile }, dispatch] = useAuthState();
 
-    const [triggerCount, setTriggerCount] = useState([]);
+  const [triggerCount, setTriggerCount] = useState([]);
 
-    const day = (24*60*60*1000)
+  const day = 24 * 60 * 60 * 1000;
 
-    useEffect(() => {
-        setTriggerCount([])
-    }, [today, dept]);
+  useEffect(() => {
+    setTriggerCount([]);
+  }, [today, dept]);
 
-    useEffect(() => {
-        if (count % 2 !== 0) {
-            setTriggerCount(prev => ([...prev,count]))
-        }
-    }, [count]);
+  useEffect(() => {
+    if (count % 2 !== 0) {
+      setTriggerCount((prev) => [...prev, count]);
+    }
+  }, [count]);
 
-    useEffect(() => {
-    const start = new Date(cols[0].label).getTime() - (day * 14)
-    const end = new Date(cols[6].label).getTime() + (day * 14)
-    const q = query(collection(db, dept), where("date", ">=", start), where("date", "<=", end), orderBy("date"))
+  useEffect(() => {
+    const start = new Date(cols[0].label).getTime() - day * 14;
+    const end = new Date(cols[6].label).getTime() + day * 14;
+    const q = query(
+      collection(db, dept),
+      where("date", ">=", start),
+      where("date", "<=", end),
+      orderBy("date")
+    );
 
+    const unsubscribe = onSnapshot(q, (qSnap) => {
+      //   console.log("Post Listener: RUNNING");
+      let obj = {};
+      qSnap.forEach((post) => {
+        obj[post.data().id] = post.data();
+      });
+      dispatch({
+        type: "SET-OBJ",
+        name: "posts",
+        load: obj,
+      });
+      // console.log(`${Object.keys(obj).length} posts returned from listener.`)
+    });
+    if (profile) {
+      return () => unsubscribe();
+    } else {
+      return () => {};
+    }
+  }, [triggerCount]);
+};
 
-    const listen = onSnapshot(q, (qSnap) => {
-        // console.log("Post Listener: RUNNING")
-        let obj = {}
-        qSnap.forEach(post => {
-            obj[post.data().id] = post.data()
-        })
-        dispatch({
-            type: "SET-OBJ",
-            name: "posts",
-            load: obj
-        })
-        // console.log(`${Object.keys(obj).length} posts returned from listener.`)
-    })
-
-    window.removeEventListener("listen", listen)
-
-        return () => {
-            window.removeEventListener("listen", listen)
-        }
-    }, [triggerCount])
-}
-
-export default usePostsListener
+export default usePostsListener;
